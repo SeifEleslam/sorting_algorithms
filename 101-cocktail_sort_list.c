e991a955034be8f71601046d861a11e8e40a4371
SeifEleslam <seifgouda98@gmail.com>
2023-12-16 07:09:16 +0200

    test

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -50,14 +50,12 @@ void cocktail_sort_list(listint_t **list)
 		while (loop)
 		{
 			compared = dir == 0 ? curr->prev : curr->next;
-			if ((curr->n <= compared->n && dir == 0) ||
-				(curr->n >= compared->n && dir != 0))
+			if ((curr->n < compared->n && dir == 0) ||
+				(curr->n > compared->n && dir != 0))
 			{
 				swap_nodes(curr, compared), swaps += 1;
-				*list = curr == *list
-							? compared
-						: compared == *list ? curr
-											: *list;
+				while ((*list)->prev)
+					*list = (*list)->prev;
 				curr = dir == 0 ? curr->next : curr->prev;
 				print_list(*list);
 			}
